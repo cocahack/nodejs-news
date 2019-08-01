63579cb3888e8810ae132520f397e2b661d6336b
Jake <cocahack@naver.com>
2019-08-01 21:00:31 +0900

    feat: OAuth 기능 추가
    Google 로그인 추가

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,234 +1,124 @@
-import { getConnection, getManager } from 'typeorm';
-import { Subscription, User } from '../entity/user.entity';
+import User, { IUser } from '../models/user.model';
+import { UserPrivilege, UserStatus } from '../types/enums';
+import { addHours } from '../util/datehelper';
 import { removeUndefinedFields } from '../util/fieldset';
 
 interface ICreateUserInput {
-  email            : User['email'];
-  password?        : User['password'];
-  provider         : User['provider'];
-  profileImageUrl? : User['profileImageUrl'];
+  email            : IUser['email'];
+  password?        : IUser['password'];
+  provider         : IUser['provider'];
+  profileImageUrl? : IUser['profileImageUrl'];
 }
 
 interface IPatchUserInput {
-  id              : User['id'];
-  email?           : User['email'];
-  password?        : User['password'];
-  privilege?       : User['privilege'];
-  profileImageUrl? : User['profileImageUrl'];
+  _id              : IUser['_id'];
+  email?           : IUser['email'];
+  password?        : IUser['password'];
+  privilege?       : IUser['privilege'];
+  profileImageUrl? : IUser['profileImageUrl'];
 }
 
-export interface IUserInformation extends User {
-  subscriberSize : number;
-}
-
-async function createUser({
+async function CreateUser({
   email,
   password,
   provider = 'local',
   profileImageUrl,
-}: ICreateUserInput): Promise<User> {
+}: ICreateUserInput): Promise<IUser> {
   try {
     const newUser = new User({
-      email, password, provider, profileImageUrl,
+      email,
+      password,
+      privilege: UserPrivilege.WRITER,
+      signUpDate: new Date(),
+      status: UserStatus.NORMAL,
+      provider,
+      profileImageUrl,
     });
-    await getManager().save(newUser);
-
+    await newUser.save();
     return newUser;
   } catch (error) {
     throw error;
   }
 }
 
-async function deleteUserById(id: User['id']): Promise<void> {
+async function GetUserById({
+  _id,
+}): Promise<IUser> {
   try {
-    await getConnection()
-      .createQueryBuilder()
-      .update(User)
-      .set({
-        deletedAt: new Date(),
-      })
-      .where(
-        'id = :id', { id, }
-      )
-      .execute();
+    const user: IUser = await User.findOne({ _id,  deletedAt: { $exists: false } }, '-password');
+    return user;
   } catch (error) {
     throw error;
   }
 }
 
-async function getUserById(id: User['id']): Promise<{ user : User, subscriberCount : number}> {
+async function GetUserByEmail(email): Promise<IUser> {
   try {
-    const connection = getConnection();
-    const user = await connection
-      .getRepository(User)
-      .createQueryBuilder('user')
-      .where('user.id = :id', { id, })
-      .andWhere('user.deletedAt is null')
-      .getOne();
-
-    /*
-    * Subscription 테이블의 별명을 sub로 지정
-    * 사용할 외래키는 subscribee
-    * subscribee가 parameter로 주어진 id와 같은 것만 고름
-    */
-    const subscriberCount = await connection
-      .getRepository(Subscription)
-      .createQueryBuilder('sub')
-      .innerJoinAndSelect(
-        'sub.subscribee',
-        'user',
-        'sub.subscribee = :id', { id, }
-      )
-      .getCount();
-
-    return { user, subscriberCount };
+    const user: IUser = await User.findOne({ email,  deletedAt: { $exists: false } }, '-password');
+    return user;
   } catch (error) {
     throw error;
   }
 }
 
-async function getUserByEmail(email: User['email']) {
+async function DeleteUserById({
+  _id,
+}): Promise<{}> {
   try {
-    return await getConnection()
-      .getRepository(User)
-      .createQueryBuilder('user')
-      .where('user.email = :email', { email, })
-      .getOne();
+    const result: {} = await User.updateOne({ _id, deletedAt: { $exists: false } }, { deletedAt: new Date() });
+    return result;
   } catch (error) {
     throw error;
   }
 }
 
-async function patchUserById(updateValues: IPatchUserInput): Promise<void> {
+async function PatchUserById({
+  _id,
+  email,
+  password,
+  privilege,
+  profileImageUrl,
+}: IPatchUserInput): Promise<any> {
   try {
-    const id = updateValues.id;
-    const refinedUpdateValues = removeUndefinedFields(updateValues);
-    // tslint:disable-next-line: no-string-literal
-    delete refinedUpdateValues['id'];
-
-    await getConnection()
-      .createQueryBuilder()
-      .update(User)
-      .set(refinedUpdateValues)
-      .where('id = :id', {id, })
-      .execute();
-
+    console.log(_id, profileImageUrl);
+    const result = await User.updateOne({
+      _id,
+      deletedAt: { $exists: false }
+    }, removeUndefinedFields({email, password, privilege, profileImageUrl}));
+    return result;
   } catch (error) {
     throw error;
   }
 }
 
-async function checkSubscribed({
-  subscriberId,
-  subscribeeId,
-}): Promise<boolean> {
-  try {
-    const subscription = await getConnection()
-      .getRepository(Subscription)
-      .createQueryBuilder('sub')
-      .where('sub.subscribeeId = :subscribeeId', { subscribeeId, })
-      .andWhere('sub.subscriberId = :subscriberId', { subscriberId, })
-      .getOne();
-    return subscription ? true : false;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function subscribeUser({
-  subscriberId,
-  writerId,
+async function banUser({
+  _id,
+  isTemporarily,
+  hours,
 }) {
   try {
-    const connection = getConnection();
+    const modifyFieldSet = isTemporarily ? {
+      status: UserStatus.BANNED_TEMPORARILY,
+      bannedExpires: addHours(hours),
+    } : {
+      status: UserStatus.BANNED_FOREVER,
+    };
+    const result = await User.updateOne(
+      { _id, deletedAt: { $exists: false }},
+      modifyFieldSet,
+    );
 
-    const subscription = await connection
-      .getRepository(Subscription)
-      .createQueryBuilder('sub')
-      .where('sub.subscribee = :subscribeeId and sub.subscriber = :subscriberId', {
-        subscriberId,
-        subscribeeId: writerId,
-      })
-      .getOne();
-
-    if (!subscription) {
-      await connection
-        .createQueryBuilder()
-        .insert()
-        .into(Subscription)
-        .values({
-          subscriber: subscriberId,
-          subscribee: writerId,
-        })
-        .execute();
-    } else {
-      await connection
-        .createQueryBuilder()
-        .update(Subscription)
-        .set({
-          deletedAt: undefined,
-        })
-        .where('subscribee = :subscribeeId and subscriber = :subscriberId', {
-          subscriberId,
-          subscribeeId: writerId,
-        })
-        .execute();
-    }
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function unsubscribeUser({
-  subscriberId,
-  writerId,
-}) {
-  try {
-    const connection = getConnection();
-    await connection
-      .createQueryBuilder()
-      .update(Subscription)
-      .set({
-        deletedAt: new Date()
-      })
-      .where('subscriber = :subscriberId and subscribee = :subscribeeId', {
-        subscriberId,
-        subscribeeId: writerId,
-      })
-      .execute();
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getSubscriptions(userId: User['id'], page = 1, pageSize = 10) {
-  try {
-    if (page < 1) page = 1;
-    return await getConnection()
-    .getRepository(Subscription)
-    .createQueryBuilder('sub')
-    .innerJoinAndSelect(
-      'sub.subscribee',
-      'user',
-    )
-    .where('sub.subscriber = :id', { id: userId, })
-    .orderBy('sub.createdAt', 'DESC')
-    .skip((page - 1) * pageSize)
-    .take(pageSize)
-    .getMany();
+    return result;
   } catch (error) {
     throw error;
   }
 }
 
 export default {
-  createUser,
-  deleteUserById,
-  getUserById,
-  patchUserById,
-  getUserByEmail,
-  checkSubscribed,
-  subscribeUser,
-  unsubscribeUser,
-  getSubscriptions,
+  CreateUser,
+  DeleteUserById,
+  GetUserById,
+  GetUserByEmail,
+  PatchUserById,
+  banUser,
 };
