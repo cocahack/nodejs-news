15992d151df692233995cb0c9ccee6c3f0b7d3f2
Jake <cocahack@naver.com>
2019-08-05 14:05:09 +0900

    feat: 구독 기능 추가
    사용자 페이지에서 버튼을 눌러 구독하는 기능 추가
    버튼은 토글처럼 동작하여, 이미 구독한 상태에서 한번 더 누르면 구독 취소됨

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -3,9 +3,7 @@ import createError from 'http-errors';
 import { RequestS3 } from '../config/multer';
 import { googleAuthUrl } from '../config/oauth';
 import articleController from '../controllers/article';
-import commentController from '../controllers/comment';
-import { Article } from '../entity/article.entity';
-import { checkArticleOwner, checkCommentOwner } from '../middlewares/article';
+import { checkArticleOwner } from '../middlewares/article';
 import { isLoggedIn } from '../middlewares/auth';
 import { articleUploadMiddleware, heroImageUploadMiddleware, markdownUploadMiddleware } from '../middlewares/upload';
 import logger from '../util/logger';
@@ -15,29 +13,16 @@ const articleRouter = Router();
 articleRouter.get('/:articleId', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const articleId = req.params.articleId;
-    const article: Article = await articleController.getArticleById(articleId, true);
-
-    let likedArticle = true;
-
-    if (req.user) {
-      likedArticle = await articleController.checkLikeArticle({articleId, likeUserId: req.user.id});
-    }
-
-    const rawMarkdown = await articleController.getMarkdown(article.markdownKey);
-
-    const commentCount = await commentController.getCommentCountOfArticle(articleId);
+    const articleInfo = await articleController.getRawArticleById(articleId);
 
     return res.render('block/article', {
       user: req.user,
-      article,
-      rawHtml: articleController.convertMarkdownToHtml(rawMarkdown),
-      writer: article.writer,
+      article: articleInfo.article,
+      rawHtml: articleInfo.rawHtml,
+      writer: articleInfo.writer,
       googleAuthUrl,
-      likedArticle,
-      commentCount,
     });
   } catch (error) {
-    console.log(error.message);
     next(createError(500));
   }
 });
@@ -46,7 +31,7 @@ articleRouter.post('/', isLoggedIn, articleUploadMiddleware,
   async (req: RequestS3, res: Response, next: NextFunction) => {
     try {
       await articleController.createArticle({
-        writerId: req.user.id,
+        writerId: req.user._id,
         title: req.body.title,
         // tslint:disable-next-line: no-string-literal
         markdownKey: req.files['markdown'][0].key,
@@ -89,7 +74,7 @@ articleRouter.get('/page/:page', async (req: Request, res: Response, next: NextF
 articleRouter.get('/manage/:page', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const page = parseInt(req.params.page, 10);
-    const articles = await articleController.getArticlesByUserId(req.user.id, page, 20);
+    const articles = await articleController.getArticlesByUserId(req.user._id, page, 20);
 
     return res.render('block/manage', { user: req.user, articles, page });
   } catch (error) {
@@ -111,7 +96,7 @@ articleRouter.delete('/:id', async (req: Request, res: Response, next: NextFunct
 
 articleRouter.patch('/:id/title', isLoggedIn, checkArticleOwner, async (req: Request, res: Response, next: NextFunction) => {
   try {
-    await articleController.patchArticleById({ id: req.params.id, title: req.body.title });
+    await articleController.patchArticleById({_id: req.params.id, title: req.body.title});
 
     return res.send();
   } catch (error) {
@@ -120,10 +105,10 @@ articleRouter.patch('/:id/title', isLoggedIn, checkArticleOwner, async (req: Req
   }
 });
 
-articleRouter.patch('/:id/markdown', isLoggedIn, checkArticleOwner, markdownUploadMiddleware,
+articleRouter.patch('/:id/markdown', isLoggedIn, markdownUploadMiddleware, checkArticleOwner,
   async (req: RequestS3, res: Response, next: NextFunction) => {
     try {
-      await articleController.patchArticleById({ id: req.params.id, markdownKey: req.file.key });
+      await articleController.patchArticleById({ _id: req.params.id, markdownKey: req.file.key });
       return res.send();
     } catch (error) {
       createError(500);
@@ -131,126 +116,15 @@ articleRouter.patch('/:id/markdown', isLoggedIn, checkArticleOwner, markdownUplo
     }
 });
 
-articleRouter.patch('/:id/heroimage', isLoggedIn, checkArticleOwner, heroImageUploadMiddleware,
+articleRouter.patch('/:id/heroimage', isLoggedIn, heroImageUploadMiddleware, checkArticleOwner,
   async (req: RequestS3, res: Response, next: NextFunction) => {
     try {
-      await articleController.patchArticleById({ id: req.params.id, heroImageUrl: req.file.location });
+      await articleController.patchArticleById({ _id: req.params.id, heroImageUrl: req.file.location });
       return res.send();
     } catch (error) {
       createError(500);
       next(error);
     }
 });
-
-articleRouter.post('/likes/:id', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await articleController.likeArticle({articleId: req.params.id, likeUserId: req.user.id});
-    res.send();
-  } catch (error) {
-    logger.error(`Fail to update like of article ${req.params.id}
-    Error Message: ${error.message}
-    Stacktrace: ${error.stack}`);
-    res.status(500).send({message: '좋아요 처리에 실패했습니다.'});
-  }
-});
-
-articleRouter.delete('/likes/:id', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await articleController.retractLikeArticle({articleId: req.params.id, likeUserId: req.user.id});
-    res.send();
-  } catch (error) {
-    logger.error(`Fail to remove like of article ${req.params.id}
-    Error Message: ${error.message}
-    Stacktrace: ${error.stack}`);
-    res.status(500).send({message: '좋아요 취소 처리에 실패했습니다.'});
-  }
-});
-
-articleRouter.get('/:id/comments/show', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const articleId = req.params.id,
-    page = req.query.page ? parseInt(req.query.page, 10) : 1,
-    userId = req.user ? req.user.id : undefined;
-    const article = await articleController.getArticleById(articleId);
-    const comments = await commentController.getComments({articleId, userId, page});
-    const commentCount = await commentController.getCommentCountOfArticle(articleId);
-
-    return res.render('block/comment', {
-      user: req.user,
-      googleAuthUrl,
-      article,
-      comments,
-      commentCount,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-articleRouter.get('/:id/comments', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const articleId = req.params.id,
-    page = req.query.page ? parseInt(req.query.page, 10) : 1,
-    userId = req.user ? req.user.id : undefined;
-    const comments = await commentController.getComments({articleId, userId, page});
-
-    return res.render('components/comment/comment-card', {
-      comments,
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(500).send({message: '댓글 조회에 실패했습니다.'});
-  }
-});
-
-articleRouter.post('/:id/comments', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const articleId = req.params.id, writerId = req.user.id;
-    await commentController.createComment({articleId, writerId, ...req.body });
-
-    res.redirect(req.headers.referer);
-  } catch (error) {
-    next(error);
-  }
-});
-
-articleRouter.delete('/:articleId/comments/:commentId', isLoggedIn, checkCommentOwner,
-  async (req: Request, res: Response) => {
-    try {
-      const { commentId } = req.params;
-      await commentController.removeComment(commentId);
-
-      res.send();
-    } catch (error) {
-      logger.error(`Error message: ${error.message}\nStacktrace: ${error.stack}`);
-      res.status(500).send({message: '댓글 삭제 도중 에러가 발생했습니다. 잠시 후 다시 시도해주세요.'});
-    }
-  });
-
-articleRouter.post('/:articleId/comments/:commentId/like', isLoggedIn,
-  async (req: Request, res: Response) => {
-    try {
-      const { commentId } = req.params;
-      await commentController.likeComment({ commentId, userId: req.user.id });
-
-      res.send();
-    } catch (error) {
-      logger.error(`Error message: ${error.message}\nStacktrace: ${error.stack}`);
-      res.status(500).send({message: '요청 처리 중 에러가 발생했습니다.'});
-    }
-  });
-
-articleRouter.delete('/:articleId/comments/:commentId/like', isLoggedIn,
-  async (req: Request, res: Response) => {
-    try {
-      const { commentId } = req.params;
-      await commentController.retractLikeComment({ commentId, userId: req.user.id });
-
-      res.send();
-    } catch (error) {
-      logger.error(`Error message: ${error.message}\nStacktrace: ${error.stack}`);
-      res.status(500).send({message: '요청 처리 중 에러가 발생했습니다.'});
-    }
-  });
 
 export default articleRouter;
